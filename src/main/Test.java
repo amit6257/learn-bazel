49e1e87b97a8cf87f895950978398e8a7c613c91
Amit Agarwal <amit6257@gmail.com>
2023-03-09 12:24:41 -0500

    main binary can depend on a target- yoohooo

--- src/main/Test.java
+++ src/main/Test.java
@@ -1,9 +1,13 @@
 package main;
 
+import mylib.TimeUtils;
+
 public class Test {
 
   public static void main(String[] args) {
     System.out.println("Hello Bazel!!!");
+    new TimeUtils().getTime();
+    System.out.println("Bye-----");
   }
 
 }
---
+++ src/mylib/TimeUtils.java
@@ -0,0 +1,11 @@
+package mylib;
+
+import java.util.Date;
+
+public class TimeUtils {
+
+  public void getTime() {
+    System.out.println(new Date());
+  }
+
+}
